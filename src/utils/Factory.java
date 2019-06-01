58f909eb4871e870e6c2a22fe852731b7e1a6d3a
LoneRaven <LoneRaven@DESKTOP-KGABTQJ>
2019-06-01 20:06:37 +0200

    Overall improvements

--- src/utils/Factory.java
+++ src/utils/Factory.java
@@ -3,46 +3,76 @@ package utils;
 import java.util.Map;
 import java.util.Scanner;
 
-import dbms.IDatabase;
-import dbms.MySqlDb;
-import entities.Book;
-import entities.LibraryDao;
-import entities.LibraryDaoImpl;
-import entities.ProxyLibrary;
+import model.dbms.IDatabase;
+import model.dbms.MySqlDb;
+import model.entities.Book;
+import model.entities.LibraryDao;
+import model.entities.LibraryDaoImpl;
+import model.entities.ProxyLibrary;
+import model.entities.User;
+import view.IView;
+import view.ViewENG;
+import view.ViewHTML;
 
 public abstract class Factory {
 	
 	static Log logger = Log.getInstance();
-	
+	// TODO check access modifiers
+	private static LibraryDao _makeLibrary(String... params) {
+		LibraryDao library = new LibraryDaoImpl(params[0], params[1], params[2], params[3]);
+		return library;
+	}
+	public static ProxyLibrary makeLibrary(String dbAddress, String schemaName, String username, String password) {
+		ProxyLibrary library =
+				new ProxyLibrary((LibraryDaoImpl)_makeLibrary(dbAddress, schemaName, username, password));
+		logger.info("Factory: New ProxyLibrary created.");
+		return library;
+	}
 	public static IDatabase makeDb(String dbAddress, String schemaName, String username, String password) {
 		MySqlDb mysqldb = new MySqlDb(dbAddress, schemaName, username, password);
-		logger.info("New MySQLDB created.");
+		logger.info("Factory: New MySQLDB created.");
 		return mysqldb;
+	}
+	
+	public static Scanner makeKbd() {
+		Scanner kbd = new Scanner(System.in);
+		logger.debug("Factory: New Scanner created.");
+		return kbd;
 	}
 	public static Book makeBook(Map<String,String> bmap) {
 		Book book = new Book(bmap.get("title"),bmap.get("author"),
 							 bmap.get("genre"),bmap.get("path"));
-		logger.info("New Book created.");
 		return book;
 	}
-	public static Book makeBook(String title, String author, String genre, String path) {
-		Book book = new Book(title,author,genre,path);
-		logger.info("New Book created.");
-		return book;
+	public static User makeUser(Map<String,String> umap) {
+		User user = new User(
+				umap.get("username"),
+				Integer.parseInt(umap.get("isAdmin")),
+				umap.get("lang")
+		);
+		logger.debug("Factory: New User created.");
+		return user;
 	}
-	private static LibraryDao _makeLibrary() {
-		LibraryDao library = new LibraryDaoImpl("localhost", "library", "root", "toor");
-		return library;
+	public static IView makeView(String path) {
+		IView v = new ViewHTML(path,new ViewENG());
+		logger.debug("Factory: New View created.");
+		return v;
 	}
-	public static ProxyLibrary makeLibrary() {
-		ProxyLibrary library =
-				new ProxyLibrary((LibraryDaoImpl)_makeLibrary());
-		logger.info("New CachedLibrary created.");
-		return library;
-	}
-	public static Scanner makeKbd() {
-		Scanner kbd = new Scanner(System.in);
-		logger.info("New Scanner created.");
-		return kbd;
-	}
+	// TODO ma tutte ste view servono??
+//	public static IView makeView() {
+//		return makeView("HTML");
+//	}
+//	public static IView makeView(String type) {
+//		IView ris = null;
+//		
+//		switch(type) {
+//			case "HTML":
+//				ris = new ViewHTML("");
+//				break;
+//			default:
+//				ris = new ViewENG();
+//		}
+//		
+//		return ris;
+//	}
 }
--- src/utils/Log.java
+++ src/utils/Log.java
@@ -6,60 +6,57 @@ public class Log
 {
 	private static Log instance;
 	private static String filePath;
-	private static boolean debug, data;
+	private static boolean debug;
 	private Log() { }
 	
-	// TODO better with getInstance(Main.class)
 	public synchronized static Log getInstance() {
-		return Log.getInstance(false,false,null);
+		return Log.getInstance(false,filePath);
 	}
-	public synchronized static Log getInstance(boolean shouldLogDebug, boolean shouldLogData, String filepaf) {
+	public synchronized static Log getInstance(boolean shouldDebug, String filepaf) {
 		if(instance == null) {
 			instance = new Log();
-			debug = shouldLogDebug;
-			data = shouldLogData;
-			if(filepaf != null)
-				filePath = filepaf;
+			debug = shouldDebug;
+			if(filepaf != null) {
+				if(!MyUtils.createLogFile(filePath = filepaf))
+					System.exit(-1);
+			}
 			else
 				throw new NullPointerException();
 		}
 		return instance;
 	}
 	
-	private boolean _logEvent(String s) {
-		MyUtils.write(filePath, s);
+	private boolean _logEvent(String type, String message, Exception e) {
+		boolean isError = type.equals("ERROR");
+		String str =  LocalDateTime.now()+
+					  " ["+type+"] "+message+
+					  (isError?(" Reason: \n"+e):"");
+		if(!isError)
+			InOut.print(str);
+		else
+			InOut.printException(str, e);
+		
+		MyUtils.write(filePath, str);
 		return true;
 	}
-	public boolean info(String e) {
-		String s = LocalDateTime.now()+" [INFO] "+e;
-		InOut.print(s);
-		return _logEvent(s);
+	
+	public boolean info(String message) {
+		return _logEvent("INFO",message,null);
 	}
-	public boolean warn(String e) {
-		String s = LocalDateTime.now()+" [WARN] "+e;
-		InOut.print(s);
-		return _logEvent(s);
+	public boolean warn(String message) {
+		return _logEvent("WARN",message,null);
 	}
-	public boolean error(String e, Exception ex) {
-		String s = LocalDateTime.now()+" [ERROR] "+e+" Reason: \n"+ex;
-		InOut.printException(e,ex);
-		return _logEvent(s);
+	public boolean error(String message, Exception e) {
+		return _logEvent("ERROR",message,e);
 	}
-	// TODO servono?
-	public boolean debug(String e) {
-		if(debug) {
-			String s = LocalDateTime.now()+" [DEBUG] "+e;
-			InOut.print(s);
-			return _logEvent(s);
-		}
+	public boolean debug(String message) {
+		if(debug)
+			return _logEvent("DEBUG",message,null);
 		return false;
 	}
-	public boolean data(String e) {
-		if(data) {
-			String s = LocalDateTime.now()+" [DATA] "+e;
-			InOut.print(s);
-			return _logEvent(s);
-		}
+	public boolean data(String message) {
+		if(debug)
+			return _logEvent("DATA",message,null);
 		return false;
 	}
 }
